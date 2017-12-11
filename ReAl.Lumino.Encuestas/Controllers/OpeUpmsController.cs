c5e168112190b14fb7935ef27dd3b481e3889136
R. Alonzo Vera A <7.re.al.7@gmail.com>
2017-12-11 11:31:18 -0400

    Se ha añadido la funcionalidad AJAX al selector de Departamento

--- ReAl.Lumino.Encuestas/Controllers/OpeUpmsController.cs
+++ ReAl.Lumino.Encuestas/Controllers/OpeUpmsController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using ReAl.Lumino.Encuestas.Models;
 
 namespace ReAl.Lumino.Encuestas.Controllers
 {
+    [Authorize]
     public class OpeUpmsController : BaseController
     {
 		public OpeUpmsController(db_encuestasContext context, IConfiguration configuration) : base(context, configuration)
