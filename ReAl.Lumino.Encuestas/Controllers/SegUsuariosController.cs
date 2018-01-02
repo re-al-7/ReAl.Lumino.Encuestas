9efa2f642e42c39726e7b5c5ad60fb695b092945
R. Alonzo Vera A <7.re.al.7@gmail.com>
2018-01-02 11:26:14 -0400

    Cambiar pagina de perfil

--- ReAl.Lumino.Encuestas/Controllers/SegUsuariosController.cs
+++ ReAl.Lumino.Encuestas/Controllers/SegUsuariosController.cs
@@ -1,31 +1,36 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Npgsql;
 using ReAl.Lumino.Encuestas.Models;
 
 namespace ReAl.Lumino.Encuestas.Controllers
 {
+    [Authorize]
     public class SegUsuariosController : BaseController
     {
-		public SegUsuariosController(db_encuestasContext context) : base(context)
+		public SegUsuariosController(db_encuestasContext context, IConfiguration configuration) : base(context, configuration)
         {
         }
 
         // GET: SegUsuarios
         public async Task<IActionResult> Index()
         {
-			var dbEncuestasContext = _context.SegUsuarios.Include(s => s.IdcdeNavigation).Include(s => s.IdobrNavigation);
-            return View(await dbEncuestasContext.ToListAsync());
+            var db_encuestasContext = _context.SegUsuarios.Include(s => s.IdcdeNavigation).Include(s => s.IdobrNavigation);
+            return View(await db_encuestasContext.ToListAsync());
         }
 
         // GET: SegUsuarios/Details/5
         public async Task<IActionResult> Details(long? id)
         {
-			if (id == null)
+			
+            if (id == null)
             {
                 return NotFound();
             }
@@ -44,9 +49,13 @@ namespace ReAl.Lumino.Encuestas.Controllers
 
         // GET: SegUsuarios/Create
         public IActionResult Create()
-        {
-			ViewData["Idcde"] = new SelectList(_context.CatDepartamentos, "Idcde", "Nombre");
-            ViewData["Idobr"] = new SelectList(_context.OpeBrigadas, "Idobr", "Codigo");
+        {			
+        ViewData["Idcde"] = new SelectList(_context.CatDepartamentos, 
+            CatDepartamentos.Fields.Idcde.ToString(), 
+            CatDepartamentos.Fields.Idcde.ToString());
+        ViewData["Idobr"] = new SelectList(_context.OpeBrigadas, 
+            OpeBrigadas.Fields.Idobr.ToString(), 
+            OpeBrigadas.Fields.Codigo.ToString());
             return View();
         }
 
@@ -61,6 +70,7 @@ namespace ReAl.Lumino.Encuestas.Controllers
             {
 				try
 				{
+                    segUsuarios.Usucre = this.GetLogin();
 					_context.Add(segUsuarios);
 					await _context.SaveChangesAsync();
                 	return RedirectToAction(nameof(Index));
@@ -71,12 +81,25 @@ namespace ReAl.Lumino.Encuestas.Controllers
                         ViewBag.ErrorDb = exp.InnerException.Message;                        
                     else
                         ModelState.AddModelError("", exp.Message);
-               					
+                    ViewData["Idcde"] = 
+                        new SelectList(_context.CatDepartamentos, 
+                        CatDepartamentos.Fields.Idcde.ToString(), 
+                        CatDepartamentos.Fields.Idcde.ToString());
+                    ViewData["Idobr"] = 
+                        new SelectList(_context.OpeBrigadas, 
+                        OpeBrigadas.Fields.Idobr.ToString(), 
+                        OpeBrigadas.Fields.Codigo.ToString());
                     return View();
                 }  
             }
-            ViewData["Idcde"] = new SelectList(_context.CatDepartamentos, "Idcde", "Nombre", segUsuarios.Idcde);
-            ViewData["Idobr"] = new SelectList(_context.OpeBrigadas, "Idobr", "Codigo", segUsuarios.Idobr);
+            ViewData["Idcde"] = new SelectList(_context.CatDepartamentos, 
+                CatDepartamentos.Fields.Idcde.ToString(), 
+                CatDepartamentos.Fields.Idcde.ToString(), 
+                segUsuarios.Idcde);
+            ViewData["Idobr"] = new SelectList(_context.OpeBrigadas, 
+                OpeBrigadas.Fields.Idobr.ToString(), 
+                OpeBrigadas.Fields.Codigo.ToString(), 
+                segUsuarios.Idobr);
             return View(segUsuarios);
         }
 
@@ -93,8 +116,14 @@ namespace ReAl.Lumino.Encuestas.Controllers
             {
                 return NotFound();
             }
-            ViewData["Idcde"] = new SelectList(_context.CatDepartamentos, "Idcde", "Nombre", segUsuarios.Idcde);
-            ViewData["Idobr"] = new SelectList(_context.OpeBrigadas, "Idobr", "Codigo", segUsuarios.Idobr);
+            ViewData["Idcde"] = new SelectList(_context.CatDepartamentos, 
+                CatDepartamentos.Fields.Idcde.ToString(), 
+                CatDepartamentos.Fields.Idcde.ToString(), 
+                segUsuarios.Idcde);
+            ViewData["Idobr"] = new SelectList(_context.OpeBrigadas, 
+                OpeBrigadas.Fields.Idobr.ToString(), 
+                OpeBrigadas.Fields.Codigo.ToString(), 
+                segUsuarios.Idobr);
             return View(segUsuarios);
         }
 
@@ -110,6 +139,93 @@ namespace ReAl.Lumino.Encuestas.Controllers
                 return NotFound();
             }
 
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    segUsuarios.Usumod = this.GetLogin();
+                    segUsuarios.Apitransaccion = "MODIFICAR";
+                    _context.Update(segUsuarios);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SegUsuariosExists(segUsuarios.Idsus))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+				catch (Exception exp)
+                {
+                    if (exp.InnerException is NpgsqlException)
+                        ViewBag.ErrorDb = exp.InnerException.Message;                        
+                    else
+                        ModelState.AddModelError("", exp.Message);
+                    ViewData["Idcde"] = 
+                        new SelectList(_context.CatDepartamentos, 
+                        CatDepartamentos.Fields.Idcde.ToString(), 
+                        CatDepartamentos.Fields.Idcde.ToString());
+                    ViewData["Idobr"] = 
+                        new SelectList(_context.OpeBrigadas, 
+                        OpeBrigadas.Fields.Idobr.ToString(), 
+                        OpeBrigadas.Fields.Codigo.ToString());
+                    return View(segUsuarios);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Idcde"] = new SelectList(_context.CatDepartamentos, 
+                CatDepartamentos.Fields.Idcde.ToString(), 
+                CatDepartamentos.Fields.Idcde.ToString(), 
+                segUsuarios.Idcde);
+            ViewData["Idobr"] = new SelectList(_context.OpeBrigadas, 
+                OpeBrigadas.Fields.Idobr.ToString(), 
+                OpeBrigadas.Fields.Codigo.ToString(), 
+                segUsuarios.Idobr);
+            return View(segUsuarios);
+        }
+
+        // GET: SegUsuarios/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var segUsuarios = await _context.SegUsuarios
+                .Include(s => s.IdcdeNavigation)
+                .Include(s => s.IdobrNavigation)
+                .SingleOrDefaultAsync(m => m.Idsus == id);
+            if (segUsuarios == null)
+            {
+                return NotFound();
+            }
+
+            return View(segUsuarios);
+        }
+
+        // GET: SegUsuarios/Profile/
+        public async Task<IActionResult> Profile()
+        {
+            var segUsuarios = await _context.SegUsuarios.SingleOrDefaultAsync(m => m.Idsus == this.GetUser().Idsus);
+            if (segUsuarios == null)
+            {
+                return NotFound();
+            }
+            return View(segUsuarios);
+        }
+
+        // POST: SegUsuarios/Profile/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Profile(long id, [Bind("Idsus,Login,Password,Nombres,Apellidos,Correo,Vigente,Idcde,Idobr,Tablet,Apiestado,Apitransaccion,Usucre,Feccre,Usumod,Fecmod")] SegUsuarios segUsuarios)
+        {
             if (ModelState.IsValid)
             {
                 try
@@ -136,33 +252,11 @@ namespace ReAl.Lumino.Encuestas.Controllers
 
                     return View(segUsuarios);
                 }
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(DashboardController.Index), "Dashboard");
             }
-            ViewData["Idcde"] = new SelectList(_context.CatDepartamentos, "Idcde", "Nombre", segUsuarios.Idcde);
-            ViewData["Idobr"] = new SelectList(_context.OpeBrigadas, "Idobr", "Codigo", segUsuarios.Idobr);
             return View(segUsuarios);
         }
-
-        // GET: SegUsuarios/Delete/5
-        public async Task<IActionResult> Delete(long? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var segUsuarios = await _context.SegUsuarios
-                .Include(s => s.IdcdeNavigation)
-                .Include(s => s.IdobrNavigation)
-                .SingleOrDefaultAsync(m => m.Idsus == id);
-            if (segUsuarios == null)
-            {
-                return NotFound();
-            }
-
-            return View(segUsuarios);
-        }
-
+        
         // POST: SegUsuarios/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -181,7 +275,12 @@ namespace ReAl.Lumino.Encuestas.Controllers
                     ViewBag.ErrorDb = exp.InnerException.Message;                        
                 else
                     ModelState.AddModelError("", exp.Message);
-            
+                ViewData["Idcde"] = new SelectList(_context.CatDepartamentos, 
+                    CatDepartamentos.Fields.Idcde.ToString(), 
+                    CatDepartamentos.Fields.Idcde.ToString());
+                ViewData["Idobr"] = new SelectList(_context.OpeBrigadas, 
+                    OpeBrigadas.Fields.Idobr.ToString(), 
+                    OpeBrigadas.Fields.Codigo.ToString());
                 return View();
             }     
         }
