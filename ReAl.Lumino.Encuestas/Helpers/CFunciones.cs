d9dba468c42ea639d4a4bdece406aeca14a0b9d1
R. Alonzo Vera A <7.re.al.7@gmail.com>
2018-01-02 11:58:33 -0400

    Funcion para obtener los tipos Mime

---
+++ ReAl.Lumino.Encuestas/Helpers/CFunciones.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ReAl.Lumino.Encuestas.Helpers
+{
+    public class CFunciones
+    {
+        public static string generarMD5(string cadena)
+        {
+            Byte[] originalBytes;
+            Byte[] encodedBytes;
+            MD5CryptoServiceProvider md5;
+
+            md5 = new MD5CryptoServiceProvider();
+            originalBytes = ASCIIEncoding.Default.GetBytes(cadena);
+            encodedBytes = md5.ComputeHash(originalBytes);
+
+            return BitConverter.ToString(encodedBytes).Replace("-", "");
+        }
+        
+        public static string GetContentType(string path)
+        {
+            var types = GetMimeTypes();
+            var ext = Path.GetExtension(path).ToLowerInvariant();
+            return types[ext];
+        }
+
+        private static Dictionary<string, string> GetMimeTypes()
+        {
+            return new Dictionary<string, string>
+            {
+                {".txt", "text/plain"},
+                {".pdf", "application/pdf"},
+                {".doc", "application/vnd.ms-word"},
+                {".docx", "application/vnd.ms-word"},
+                {".xls", "application/vnd.ms-excel"},
+                {".xlsx", "application/vnd.openxmlformats"},
+                {".png", "image/png"},
+                {".jpg", "image/jpeg"},
+                {".jpeg", "image/jpeg"},
+                {".gif", "image/gif"},
+                {".csv", "text/csv"}
+            };
+            }
+    }
+}
