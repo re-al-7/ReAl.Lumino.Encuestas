342c07fb562b0a97838c717df839807c6bfc3504
R. Alonzo Vera A <7.re.al.7@gmail.com>
2018-01-04 11:31:28 -0400

    Exportar Reportes con EPPlus

--- ReAl.Lumino.Encuestas/Controllers/ReportesController.cs
+++ ReAl.Lumino.Encuestas/Controllers/ReportesController.cs
@@ -4,10 +4,21 @@
 // // <author>re-al </author>
 // // <date>2017-12-29 9:24</date>
 
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
+using Npgsql;
+using OfficeOpenXml;
+using OfficeOpenXml.Table;
 using ReAl.Lumino.Encuestas.Dal;
 using ReAl.Lumino.Encuestas.Helpers;
 using ReAl.Lumino.Encuestas.Models;
@@ -30,5 +41,41 @@ namespace ReAl.Lumino.Encuestas.Controllers
             ViewData["datos"] = dtReporte;
             return View();
         }
+        
+        /// <summary>
+        /// An in-memory report
+        /// </summary>
+        public IActionResult Download()
+        {
+            byte[] reportBytes;
+            var rn = new RnVista(_connectionStringsSettings.Value);
+            var arrColWhere = new ArrayList {OpeProyectos.Fields.Idopy.ToString()};
+            var arrValWhere = new ArrayList {this.GetProyectoId()};
+            var dtReporte = rn.ObtenerDatos("vw_enc_flujo", arrColWhere, arrValWhere);
+
+            foreach (DataColumn column in dtReporte.Columns)
+                column.ColumnName = column.ColumnName.ToPascalCase();
+            
+            using (var package = new ExcelPackage())
+            {
+                ExcelWorksheet ws = package.Workbook.Worksheets.Add("Reporte");
+                //Titulos
+                ws.Cells["A1"].Value = "Reporte";
+                ws.Cells["A2"].Value = "Fecha: " + DateTime.Now;
+                ws.Cells["A3"].Value = "Elaborado por " + this.GetUser().Nombres + " " + this.GetUser().Apellidos;
+                ws.Cells[1, 1, 3, 1].Style.Font.Bold = true;
+                
+                //Reporte
+                ws.Cells["A6"].LoadFromDataTable(dtReporte, true);                
+                ws.Cells[6, 1, dtReporte.Rows.Count, dtReporte.Columns.Count].AutoFitColumns();
+                
+                var tbl = ws.Tables.Add(new ExcelAddressBase(fromRow: 6, fromCol: 1, toRow: dtReporte.Rows.Count, toColumn: dtReporte.Columns.Count), "Data");
+                tbl.ShowHeader = true;                
+                
+                reportBytes = package.GetAsByteArray();                 
+            }
+
+            return File(reportBytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "report.xlsx");
+        }       
     }
 }
---
+++ ReAl.Lumino.Encuestas/Helpers/ExtensionMethods.cs
@@ -0,0 +1,34 @@
+﻿// // <copyright file="ExtensionMethods.cs" company="INTEGRATE - Soluciones Informaticas">
+// // Copyright (c) 2016 Todos los derechos reservados
+// // </copyright>
+// // <author>re-al </author>
+// // <date>2018-01-04 11:27</date>
+
+using System;
+using System.Linq;
+
+namespace ReAl.Lumino.Encuestas.Helpers
+{
+    public static class ExtensionMethods
+    {
+        public static string ToUnderscoreCase(this string str)
+        {
+            return string.Concat(str.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString())).ToLower();
+        }
+
+        public static string ToCamelCase(this string str)
+        {
+            return str.Split(new[] { "_" }, StringSplitOptions.RemoveEmptyEntries).Select(s => char.ToUpperInvariant(s[0]) + s.Substring(1, s.Length - 1)).Aggregate(string.Empty, (s1, s2) => s1 + s2);
+        }
+
+        public static string ToPascalCase(this string str)
+        {
+            var words = str.Split(new[] { '-', '_' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(word => word.Substring(0, 1).ToUpper() +
+                                word.Substring(1).ToLower());
+
+            var result = String.Concat(words);
+            return result;
+        }
+    }
+}
